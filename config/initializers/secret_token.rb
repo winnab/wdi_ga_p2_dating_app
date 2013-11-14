420561e907553dc898328b79542c1d2be021d520
winnab <winnab@gmail.com>
2013-11-14 20:30:14 +0000

    created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WdiGaP2DatingApp::Application.config.secret_token = '3296b428794247e0a1ecdd202074aeb84ecf29e3c3d9e9fa31b9c89d1c47c249c130d12a6d90b83c0de1288b6708af00f2a367e50df2f08e91efde3b8062d903'
