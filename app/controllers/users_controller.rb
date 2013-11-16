cc5fb40e1f4aac5a8ffdbf7382015b1b8a630d34
winnab <winnab@gmail.com>
2013-11-16 20:24:19 +0000

    updated controllers and forms for devise. updated locales file for custom registrations info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
-class UsersController < ApplicationController
+class UsersController < Devise::RegistrationsController
   
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, except: [:index, :show]
+  
   # user_signed_in? => devise helper
   # user_session => devise helper
 
