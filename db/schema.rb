488e9668d99c7d0077b6c0aebb9ee19797d165b7
odlp <oliverp@gmail.com>
2013-11-19 14:18:08 +0000

    reinstated migration to add avatar column on Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131115165822) do
+ActiveRecord::Schema.define(:version => 20131119141422) do
 
   create_table "events", :force => true do |t|
     t.integer  "user_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20131115165822) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "avatar"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
