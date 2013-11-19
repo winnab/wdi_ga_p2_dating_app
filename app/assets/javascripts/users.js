35ccfc0517202310d088c1b4f992ad6c2e7f4ea9
odlp <oliverp@gmail.com>
2013-11-19 19:41:35 +0000

    refactor buttons to hyperlink to allow graceful degrading with no-script

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,12 @@
 $(function() {
 
-  function toggleUserAction() {
-    $this      = $(this);
-    var url    = "/users/" + $this.attr('data-id') + "/" + $this.attr('data-action');
-    $.get(url, function(response) {$this.replaceWith(response)})
+  function toggleUserAction(ele) {
+    var $this = $(ele);
+    $.get($this.attr('href'), function(response) {$this.replaceWith(response)})
   }
 
-  $('#star-action, #flag-action, #poke-action').on('click', 'button', toggleUserAction);
+  $('#star-action, #flag-action, #poke-action').on('click', 'a', function(event) {
+    event.preventDefault();
+    toggleUserAction(this);
+  });
 })
