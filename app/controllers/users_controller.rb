51331f9ce143149935db85f6c05d117a4369ce18
SophGits <sophie.jiffy@gmail.com>
2013-11-20 15:48:30 +0000

    6 passing tests, 1 in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show, :do_search]
+  # before_filter :authenticate_user!, except: [:index, :show, :do_search]
 
   def dashboard
     @news_items     = current_user.news_items
