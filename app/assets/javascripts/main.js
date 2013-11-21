1c513d9b37e168d3404a5ad44efa83e63303c9f5
sharifzubi <szubi@me.com>
2013-11-21 15:17:34 +0000

    home form ageslider complete

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,5 +9,20 @@ $(function() {
   // show any intial alerts
   $('.alert').each(function() {
     if ($(this).text().length > 0){window.flashAlert($(this))}
-  })
+  });
+
+  $("#slider-range").slider({
+    range: true,
+    min: 18,
+    max: 80,
+    values: [18, 35],
+    slide: function(event, ui) {
+      $("#start_age").val(ui.values[0]);
+      $("#end_age").val(ui.values[1]);
+
+      $("#range").html(ui.values[0] + " - " + ui.values[1]);
+    }
+  });
+
+  $("#range").html($("#slider-range").slider("values", 0) + " - " + $("#slider-range").slider("values", 1));
 })
