f2f889f313002e2b838a52eb7a19dd347467f041
odlp <oliverp@gmail.com>
2013-11-22 11:17:52 +0000

    user can now update their qualities

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,10 +12,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
         Quality.create(user_id: @user.id, scope: "Self", quality_type: quality.first, value: quality.last) if quality.last.present?
       end
     end
-    
   end
 
-
-
+  def update
+    super
+    if @user.save
+      # remove all previous qualities of this user
+      @user.qualities.destroy_all 
+      # reset quality values
+      params[:quality].each do |quality|
+        Quality.create(user_id: @user.id, scope: "Self", quality_type: quality.first, value: quality.last) if quality.last.present?
+      end
+    end
+  end
 
 end
